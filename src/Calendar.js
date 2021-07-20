e5ed20ac049cef75d1380817d4bd54ff586dd93d
Summer Tang <summer_tang@trendmicro.com>
2021-07-20 17:36:38 +0800

    feat: add utils

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,45 +1,30 @@
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import { 
+  useState,
+  useEffect,
+} from 'react';
+import PropTypes from 'prop-types';
 import {
+  Flex,
+  Grid,
   useColorMode,
   useColorStyle,
-  Icon,
-  Grid,
-  Box,
-  Button,
-  Flex
-} from "@trendmicro/react-styled-ui";
+} from '@trendmicro/react-styled-ui';
 import {
   DAY,
   DEFAULT_WEEK_ROW,
-  FEB_OF_LEAP_YEAR,
-  MONTH_DAYS,
-  MONTH,
   WEEK_CONFIG,
 } from './constants';
+import {
+  dateToAry,
+  dateToObj,
+  dateToStr,
+  getMonthDays,
+  getPrevMonthLastDate,
+  isValidDate,
+} from './utils';
 import Controller from './Controller';
 import Week from './Week';
 import WeekTitle from './WeekTitle';
-
-export const dateToAry = (rawDate) => {
-  const newDate = new Date(rawDate);
-  const [month, date, year] = newDate.toLocaleDateString("en-US").split("/");
-  return [+year, +month, +date];
-};
-
-export const dateToObj = (rawDate) => {
-  const [year, month, date] = dateToAry(rawDate);
-  return { year, month, date };
-};
-
-export const dateToStr = (rawDate, full = false) => {
-  const newDate = dateToAry(rawDate);
-  return newDate
-    .map((item) => (full && item < 10 ? `0${item}` : item))
-    .join("-");
-};
-
-const isValidDate = (dateStr) => !!new Date(dateStr).toJSON();
 
 const Calendar = ({ startDate: rawDate, onSelect }) => {
   const today = new Date();
@@ -47,7 +32,6 @@ const Calendar = ({ startDate: rawDate, onSelect }) => {
   const initDate = isValidDate(rawDate) ? rawDate : today;
   const [renderDate, setRenderDate] = useState(dateToObj(initDate));
   const [selectedDateStr, setSelectedDateStr] = useState(dateToStr(initDate));
-
   const [colorMode] = useColorMode();
   const [colorStyle] = useColorStyle({ colorMode });
 
@@ -61,18 +45,6 @@ const Calendar = ({ startDate: rawDate, onSelect }) => {
 
   const getDays = ({ year, month }) => {
     const days = [];
-
-    const isLeapYear = (checkYear) =>
-      new Date(checkYear, 1, FEB_OF_LEAP_YEAR).getDate() === FEB_OF_LEAP_YEAR;
-    const getMonthDays = (year, month) =>
-      isLeapYear(year) && month === 2
-        ? FEB_OF_LEAP_YEAR
-        : MONTH_DAYS[month - 1];
-    const getPrevMonthLastDate = (curDate) => {
-      curDate.setDate(0);
-      return curDate;
-    };
-
     const daysLength = DEFAULT_WEEK_ROW * DAY.length;
     const curMonthDays = getMonthDays(year, month);
     const curMonthFirstDay = new Date(`${year}-${month}-01`).getDay();
