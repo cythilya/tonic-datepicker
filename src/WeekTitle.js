2586fbda0e071b69ff9e3ff28e5d0bef0b8268b4
Summer Tang <summer_tang@trendmicro.com>
2021-07-20 17:18:35 +0800

    refactor: refactor WeekTitle component - text indent for 2 spaces

--- src/WeekTitle.js
+++ src/WeekTitle.js
@@ -2,18 +2,18 @@ import { Flex } from "@trendmicro/react-styled-ui";
 import { DAY } from './constants';
 
 const WeekTitle = () => (
-    <>
-        {DAY.map((dayName, idx) => (
-            <Flex
-                height="10x"
-                align="center"
-                justify="center"
-                key={`${dayName}${idx}`}
-            >
-                {dayName}
-            </Flex>
-        ))}
-    </>
+  <>
+    {DAY.map((dayName, idx) => (
+      <Flex
+        height="10x"
+        align="center"
+        justify="center"
+        key={`${dayName}${idx}`}
+      >
+        {dayName}
+      </Flex>
+    ))}
+  </>
 );
 
 export default WeekTitle;
